15eb877c799d2d9daf5b7baa6418c91b1ffdf007
Ronan <ronan.poinsignon@laposte.net>
2022-04-26 23:52:38 +0200

    suppression code inutile

--- src/main/java/appli/AppliFx.java
+++ src/main/java/appli/AppliFx.java
@@ -8,12 +8,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import service.FileManager;
 import service.ServiceManager;
-import service.SocketService;
 
 public class AppliFx extends Application {
 
 	private FileManager fm = ServiceManager.getInstance(FileManager.class);
-	private SocketService socketService = ServiceManager.getInstance(SocketService.class);
 
 	public static void start(String[] args) {
 		Application.launch(args);
@@ -21,33 +19,6 @@ public class AppliFx extends Application {
 
 	@Override
 	public void start(Stage stage) throws IOException {
-		var th = new Thread(() -> {
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e2) {
-				// TODO Auto-generated catch block
-				e2.printStackTrace();
-			}
-			while(true) {
-				try {
-					var t = "['type' => 'tracker:players','players' => ['a5Gwv5NZxa6WVtJKOFBy8sX7uBagkv0pODShPv9IjPTtslE', 'l2b1wQIobGI3FfnM_HM_9ppRWE9WTip-v3hhbpOX2MX3mFA', 'lEMlQTR5NnNM61BLdvLNVvgHVqDpUO8pTCewgSGyD42wXGc']]";
-					System.out.println("write avant");
-					socketService.write(t);
-					System.out.println("write apres");
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
-		th.setDaemon(true);
-		//th.start();
-
 		stage.getIcons().add(fm.getImageFromResource("images/loupe.PNG"));
 		stage.setTitle("Player tracker");
 		var file = fm.getFileFromResources("fxml/page_principale.fxml");
