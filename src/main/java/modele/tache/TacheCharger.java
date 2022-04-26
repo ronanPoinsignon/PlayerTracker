28d00fdef7547d2c6f9679a6e49fe118548058ce
Ronan <ronan.poinsignon@laposte.net>
2022-04-26 22:03:23 +0200

    maj du chargement des joueurs

--- src/main/java/modele/tache/TacheCharger.java
+++ src/main/java/modele/tache/TacheCharger.java
@@ -7,16 +7,14 @@ import modele.joueur.JoueurFx;
 import service.GestionnaireCommandeService;
 import service.NoPlayerFoundException;
 import service.ServiceManager;
-import service.WebService;
 
 /**
- * Classe permettant de charger une ou plusieurs vidéos dans la liste de vidéo.
+ * Classe permettant de charger un ou plusieurs joueurs dans la liste de joueurs.
  * @author ronan
  *
  */
-public class TacheCharger extends Tache<JoueurFx>{
+public class TacheCharger extends Tache<JoueurFx> {
 
-	WebService webService = ServiceManager.getInstance(WebService.class);
 	GestionnaireCommandeService gestionnaireCommandeService = ServiceManager.getInstance(GestionnaireCommandeService.class);
 	private String nom;
 	private String pseudo;
@@ -30,11 +28,6 @@ public class TacheCharger extends Tache<JoueurFx>{
 	protected JoueurFx call() throws NoPlayerFoundException, IOException {
 		Joueur joueur = new Joueur(nom, pseudo);
 		updateMessage("chargement de " + joueur.getAppellation());
-		setId(joueur);
 		return new JoueurFx(joueur);
-	}
-
-	private void setId(Joueur joueur) throws NoPlayerFoundException {
-		joueur.setId(webService.getIdFromPseudo(joueur.getPseudo()));
 	}
 }
