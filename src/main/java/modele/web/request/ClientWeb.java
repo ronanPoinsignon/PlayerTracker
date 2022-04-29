8de3d06bf85bb6ebc51c1a95035a4eb30b413f87
Ronan <ronan.poinsignon@laposte.net>
2022-04-29 02:37:32 +0200

    ajout d'une requete post pour pouvoir recuperer plusieurs joueurs en jeu

--- src/main/java/modele/web/request/ClientWeb.java
+++ src/main/java/modele/web/request/ClientWeb.java
@@ -2,8 +2,12 @@ package modele.web.request;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class ClientWeb<T> {
 
@@ -22,6 +26,29 @@ public class ClientWeb<T> {
 				.build();
 
 		return client.send(request, new JsonBodyHandler<>(clazz)).body().get();
+	}
+
+	public T post(String url, Map<Object, Object> data) throws IOException, InterruptedException {
+		var client = HttpClient.newHttpClient();
+
+		var request = HttpRequest.newBuilder()
+				.POST(ClientWeb.buildFormDataFromMap(data))
+				.uri(URI.create(url))
+				.header("Content-Type", "application/x-www-form-urlencoded")
+				.build();
+
+		return client.send(request, new JsonBodyHandler<>(clazz)).body().get();
+	}
+
+	private static HttpRequest.BodyPublisher buildFormDataFromMap(Map<Object, Object> data) {
+		var builder = new StringBuilder("&");
+		for (Entry<Object, Object> entry : data.entrySet()) {
+			builder.append(URLEncoder.encode(entry.getKey().toString(), StandardCharsets.UTF_8));
+			builder.append("=");
+			builder.append(URLEncoder.encode(entry.getValue().toString(), StandardCharsets.UTF_8));
+		}
+		System.out.println(builder.toString());
+		return HttpRequest.BodyPublishers.ofString(builder.toString());
 	}
 
 }
