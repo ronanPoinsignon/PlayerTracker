58b7ff627bf8633f2b4c1fa6f74503100c4e78bf
Ronan <ronan.poinsignon@laposte.net>
2022-04-30 14:13:54 +0200

    ajout d'un service appelant ce runnable

---
+++ src/main/java/service/WebRequestScheduler.java
@@ -0,0 +1,32 @@
+package service;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import modele.joueur.Joueur;
+import modele.scheduler.WebRequestRunnable;
+
+public class WebRequestScheduler implements IService {
+
+	private ScheduledExecutorService scheduler;
+	private WebRequestRunnable runnable;
+
+	public WebRequestScheduler() {
+		scheduler = Executors.newScheduledThreadPool(1);
+		runnable = new WebRequestRunnable();
+		scheduler.scheduleWithFixedDelay(runnable, 0, 1, TimeUnit.MINUTES);
+	}
+
+	public void addJoueur(Joueur joueur) {
+		runnable.add(joueur);
+	}
+
+	public void removeJoueur(Joueur joueur) {
+		runnable.remove(joueur);
+	}
+
+	public void stopScheduler() {
+		scheduler.shutdownNow();
+	}
+}
