82ee2fc161d9d3b127d996c2972bc8a3fd7b1e3a
Ronan <ronan.poinsignon@laposte.net>
2022-04-30 13:24:22 +0200

    MAJ des appels des getters/setters

--- src/main/java/modele/request/data/SummonerInGame.java
+++ src/main/java/modele/request/data/SummonerInGame.java
@@ -2,68 +2,78 @@ package modele.request.data;
 
 public class SummonerInGame {
 
-	private String summonerId;
-	private String summonerName;
-	private boolean inGame;
-	private String gameId;
-	private String encryptionKey;
+	private String summoner_id;
+	private String summoner_name;
+	private boolean in_game;
+	private String game_id;
+	private String encryption_key;
+	private String game_type;
 
 	public SummonerInGame() {
 
 	}
 
-	public SummonerInGame(String summonerId, String summonerName, boolean inGame, String gameId, String encryptionKey) {
-		this.summonerId = summonerId;
-		this.summonerName = summonerName;
-		this.inGame = inGame;
-		this.gameId = gameId;
-		this.encryptionKey = encryptionKey;
+	public SummonerInGame(String summoner_id, String summoner_name, boolean in_game, String game_id, String encryption_key, String game_type) {
+		this.summoner_id = summoner_id;
+		this.summoner_name = summoner_name;
+		this.in_game = in_game;
+		this.game_id = game_id;
+		this.encryption_key = encryption_key;
+		this.game_type = game_type;
 	}
 
-	public String getSummonerId() {
-		return summonerId;
+	public String getSummoner_id() {
+		return summoner_id;
 	}
 
-	public void setSummonerId(String summonerId) {
-		this.summonerId = summonerId;
+	public void setSummoner_id(String summonerId) {
+		summoner_id = summonerId;
 	}
 
-	public String getSummonerName() {
-		return summonerName;
+	public String getSummoner_name() {
+		return summoner_name;
 	}
 
-	public void setSummonerName(String summonerName) {
-		this.summonerName = summonerName;
+	public void setSummoner_name(String summonerName) {
+		summoner_name = summonerName;
 	}
 
-	public boolean isInGame() {
-		return inGame;
+	public boolean isIn_game() {
+		return in_game;
 	}
 
-	public void setInGame(boolean inGame) {
-		this.inGame = inGame;
+	public void setIn_game(boolean inGame) {
+		in_game = inGame;
 	}
 
-	public String getGameId() {
-		return gameId;
+	public String getGame_id() {
+		return game_id;
 	}
 
-	public void setGameId(String gameId) {
-		this.gameId = gameId;
+	public void setGame_id(String gameId) {
+		game_id = gameId;
 	}
 
-	public String getEncryptionKey() {
-		return encryptionKey;
+	public String getEncryption_key() {
+		return encryption_key;
 	}
 
-	public void setEncryptionKey(String encryptionKey) {
-		this.encryptionKey = encryptionKey;
+	public void setEncryption_key(String encryptionKey) {
+		encryption_key = encryptionKey;
+	}
+
+	public String getGame_type() {
+		return game_type;
+	}
+
+	public void setGame_type(String game_type) {
+		this.game_type = game_type;
 	}
 
 	@Override
 	public String toString() {
-		return "SummonerInGame [summonerId=" + summonerId + ", summonerName=" + summonerName + ", inGame=" + inGame
-				+ ", gameId=" + gameId + ", encryptionKey=" + encryptionKey + "]";
+		return "SummonerInGame [summonerId=" + summoner_id + ", summonerName=" + summoner_name + ", inGame=" + in_game
+				+ ", gameId=" + game_id + ", encryptionKey=" + encryption_key + "]";
 	}
 
 }
