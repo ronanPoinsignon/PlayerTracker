201dfc8492f20ead55caea8d6532c3400fa26451
Ronan <ronan.poinsignon@laposte.net>
2022-05-06 12:24:56 +0200

    creation d'un serverSocket pour garantir l'unicité de l'app sur windows

--- src/main/java/appli/AppliFx.java
+++ src/main/java/appli/AppliFx.java
@@ -1,11 +1,14 @@
 package appli;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import service.AlertFxService;
 import service.FileManager;
 import service.ServiceManager;
 import service.TrayIconService;
@@ -14,6 +17,7 @@ public class AppliFx extends Application {
 
 	private FileManager fm = ServiceManager.getInstance(FileManager.class);
 	private TrayIconService trayIconService = ServiceManager.getInstance(TrayIconService.class);
+	private AlertFxService alertService = ServiceManager.getInstance(AlertFxService.class);
 
 	public static void start(String[] args) {
 		Application.launch(args);
@@ -21,6 +25,12 @@ public class AppliFx extends Application {
 
 	@Override
 	public void start(Stage stage) throws IOException {
+		try {
+			checkAlreadyRunning();
+		} catch (ApplicationDejaEnCoursException e) {
+			alertService.alert(e);
+			return;
+		}
 		stage.getIcons().add(fm.getImageFromResource("images/loupe.PNG"));
 		stage.setTitle("Player tracker");
 		var file = fm.getFileFromResources("fxml/page_principale.fxml");
@@ -30,6 +40,23 @@ public class AppliFx extends Application {
 		stage.setScene(scene);
 		trayIconService.createFXTrayIcon(stage);
 		stage.show();
+	}
+
+	private void checkAlreadyRunning() throws ApplicationDejaEnCoursException {
+		try(var s = new ServerSocket(1044, 0, InetAddress.getByName("localhost"));) {
+			// teste juste la possibilité d'ouvrir un serveur à cette adresse
+		} catch (IOException e1) {
+			throw new ApplicationDejaEnCoursException();
+		}
+		var t = new Thread(() -> {
+			try(var server = new ServerSocket(1044, 0, InetAddress.getByName("localhost"));) {
+				server.accept();
+			} catch (IOException e) {
+				alertService.alert(e);
+			}
+		});
+		t.setDaemon(true);
+		t.start();
 	}
 
 }
