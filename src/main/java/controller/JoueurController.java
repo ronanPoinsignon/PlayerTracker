faa20287b42918ed6502c1fcdd77c74d4a5028ee
Jérémy Huet <huetjeremy@outlook.com>
2022-05-11 00:03:53 +0200

    Fix temporaire de mise à jour graphique du statut

--- src/main/java/controller/JoueurController.java
+++ src/main/java/controller/JoueurController.java
@@ -3,6 +3,9 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -29,25 +32,38 @@ public class JoueurController implements Initializable {
 	private ImageView image_statut;
 	
 	private SimpleObjectProperty<JoueurFx> joueur = new SimpleObjectProperty<>();
+	private BooleanProperty isConnecte = new SimpleBooleanProperty();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		this.joueur.addListener((obs, oldV, newV) -> {
 			nom.setText(newV.getNom()+" ("+newV.getPseudo()+")");
-			
-			if(newV.getIsConnecteProperty().getValue()) {
-				statut.setText("En jeu");
+						
+			image_statut.imageProperty().bind(newV.getImageConnexion());
+		});
+				
+		this.isConnecte.addListener((obs, oldV, newV) -> {
+			if(newV) {
+				Platform.runLater(() -> statut.setText("En jeu"));
 			}
 			else {
-				statut.setText("Déconnecté");
+				Platform.runLater(() -> statut.setText("Déconnecté"));
 			}
-			
-			image_statut.imageProperty().bind(newV.getImageConnexion());
 		});
 	}
 	
 	public void setJoueur(JoueurFx joueur) {		
-		this.joueur.set(joueur);		
+		this.joueur.set(joueur);
+		
+		this.isConnecte.unbind();
+		this.isConnecte.bind(joueur.getIsConnecteProperty());
+		
+		if(joueur.isInGame()) {
+			Platform.runLater(() -> statut.setText("En jeu"));
+		}
+		else {
+			Platform.runLater(() -> statut.setText("Déconnecté"));
+		}
 	}
 
 }
