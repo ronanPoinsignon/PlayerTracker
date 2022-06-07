a4a8cc5f3da85555892e29aa660882dd22075d64
Jérémy Huet <huetjeremy@outlook.com>
2022-06-07 16:46:13 +0200

    Mise en place d'une scrollbar

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
 import modele.affichage.PaneViewElement;
@@ -13,6 +15,9 @@ import modele.event.eventaction.AddEvent;
 public class MainController implements Initializable {
 	@FXML
 	private AnchorPane anchor;
+	
+	@FXML
+	private ScrollPane scrollpane;
 	
 	private PaneViewElement joueursContainer;
 	
@@ -24,18 +29,37 @@ public class MainController implements Initializable {
 		joueursContainer = new PaneViewElement();
 		
 		anchor.getChildren().add(joueursContainer);
+
+		joueursContainer.heightProperty().addListener((obs, oldValue, newValue) -> {
+			anchor.setPrefHeight(newValue.doubleValue());
+		});
+		
+		scrollpane.setHbarPolicy(ScrollBarPolicy.NEVER);
+		
+		if(anchor.getHeight() > scrollpane.getMinHeight())
+			scrollpane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+		else
+			scrollpane.setVbarPolicy(ScrollBarPolicy.NEVER);
+
+		anchor.heightProperty().addListener((obs,oldValue, newValue) -> {
+			if(newValue.doubleValue() > scrollpane.getMinHeight())
+				scrollpane.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+			else
+				scrollpane.setVbarPolicy(ScrollBarPolicy.NEVER);
+		});
 				
 		openModal();
 	}
 	
 	@FXML
 	public void openModal() {
-		String[] nom = new String[] {"Théo"};
-		String[] pseudo = new String[] {"TheYoloToto"};
+		String[] nom = new String[] {"Théo", "Test", "Théo", "Test", "Théo", "Test", "Théo", "Test", "Théo", "Test"};
+		//nom = new String[] {"Théo", "Test"};
+		String[] pseudo = new String[] {"ruep3", "Guerinoob", "ronan3290", "TheYoloToto", "Makishimu M", "Pléxi", "sanchodecubah", "65 ms player", "deathkay", "SitSonXD"};
 		
 		Thread t;
 		
-		for(int i = 0; i < 1; i++) {
+		for(int i = 0; i < nom.length; i++) {
 			t = new Thread(new AddEvent(joueursContainer, nom[i], pseudo[i]));
 			t.setDaemon(true);
 			t.start();
