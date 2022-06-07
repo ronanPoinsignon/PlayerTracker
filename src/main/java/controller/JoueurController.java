0bedc8ed84c0d5c48c422b0dbf55c02a7b70d679
Jérémy Huet <huetjeremy@outlook.com>
2022-06-07 16:46:35 +0200

    Affichage du champion joué

--- src/main/java/controller/JoueurController.java
+++ src/main/java/controller/JoueurController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import java.io.ByteArrayInputStream;
 import java.net.URL;
+import java.util.Base64;
+import java.util.Base64.Decoder;
 import java.util.ResourceBundle;
 
 import javafx.application.Platform;
@@ -10,6 +13,7 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import modele.joueur.JoueurFx;
@@ -35,15 +39,30 @@ public class JoueurController implements Initializable {
 	private BooleanProperty isConnecte = new SimpleBooleanProperty();
 
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+	public void initialize(URL location, ResourceBundle resources) {		
 		joueur.addListener((obs, oldV, newV) -> {
 			nom.setText(newV.getNom() + " (" + newV.getPseudo() + ")");
 
 			imageStatut.imageProperty().bind(newV.getImageConnexion());
 		});
 
-		isConnecte.addListener((obs, oldV, newV) 
-				-> Platform.runLater(() ->	statut.setText(Boolean.TRUE.equals(newV) ? "En jeu" : "Déconnecté")));
+		isConnecte.addListener((obs, oldV, newV) -> {
+			Image tempImage = null;
+			
+			if(newV.booleanValue()) {
+				Decoder decoder = Base64.getDecoder();
+				byte[] imageBytes = decoder.decode(joueur.get().getPartie().getChampion().getBase64ChampionImage());
+				ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
+				tempImage = new Image(bis);
+			}
+			
+			final Image image = tempImage;
+			
+			Platform.runLater(() ->	{
+				statut.setText(Boolean.TRUE.equals(newV) ? "En jeu" : "Déconnecté");
+				imageJoueur.setImage(newV.booleanValue() ? image : null);
+			});
+		});
 	}
 
 	public void setJoueur(JoueurFx joueur) {
