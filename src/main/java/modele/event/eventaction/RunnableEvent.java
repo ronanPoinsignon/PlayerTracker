8cb666aa481d350a195373a0fce83aa4bddf4c71
Ronan <ronan.poinsignon@laposte.net>
2022-07-24 12:07:41 +0200

    certaines erreurs n'étaient pas remontées à l'utilisateur

--- src/main/java/modele/event/eventaction/RunnableEvent.java
+++ src/main/java/modele/event/eventaction/RunnableEvent.java
@@ -1,9 +1,18 @@
 package modele.event.eventaction;
 
+import service.AlertFxService;
+import service.ServiceManager;
+
 public abstract class RunnableEvent extends EventAction<Void> implements Runnable {
+
+	AlertFxService alerteService = ServiceManager.getInstance(AlertFxService.class);
 
 	@Override
 	public void run() {
-		execute();
+		try {
+			execute();
+		} catch (final Exception e) {
+			alerteService.alert(e);
+		}
 	}
 }
