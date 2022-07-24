b58d743273b7dc47af844ebe00799b21b5752984
Ronan <ronan.poinsignon@laposte.net>
2022-07-24 12:07:56 +0200

    correction d'un pb d'affichage

--- src/main/java/modele/event/eventaction/AddEvent.java
+++ src/main/java/modele/event/eventaction/AddEvent.java
@@ -33,7 +33,7 @@ public class AddEvent extends RunnableEventWithTable<JoueurFx> {
 			return null;
 		}
 		if(table.getItems().stream().anyMatch(joueur -> pseudo.equals(joueur.getPseudo()))) {
-			throw new JoueurDejaPresentException(nom);
+			throw new JoueurDejaPresentException(pseudo);
 		}
 		final var tache = new TacheCharger(nom, pseudo, serveur);
 		tache.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED, t -> {
