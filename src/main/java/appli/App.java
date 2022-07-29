ec2f5451ea55ce9a3f7dd4eff12003f821403fb6
Jérémy Huet <huetjeremy@outlook.com>
2022-07-29 17:07:17 +0200

    Nouvelle classe Application

--- src/main/java/appli/App.java
+++ src/main/java/appli/App.java
@@ -3,6 +3,7 @@ package appli;
 public class App {
 
 	public static void main(String[] args) {
-		AppliFx.start(args);
+		//AppliFx.start(args);
+		AppliNew.start(args);
 	}
 }
---
+++ src/main/java/appli/AppliNew.java
@@ -0,0 +1,61 @@
+package appli;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import service.AlertFxService;
+import service.FileManager;
+import service.ServiceManager;
+import service.TrayIconService;
+
+public class AppliNew extends Application {
+	
+	private FileManager fm = ServiceManager.getInstance(FileManager.class);
+	private TrayIconService trayIconService = ServiceManager.getInstance(TrayIconService.class);
+	private AlertFxService alertService = ServiceManager.getInstance(AlertFxService.class);
+
+	public static void start(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws IOException {
+		stage.getIcons().add(fm.getImageFromResource("images/loupe.PNG"));
+		stage.setTitle("Player tracker");
+		stage.setResizable(false);
+		
+		var file = fm.getFileFromResources("fxml/main.fxml");
+		var loader = new FXMLLoader(file.toURI().toURL());
+		Parent sceneVideo = loader.load();
+		
+		var scene = new Scene(sceneVideo);
+		scene.getStylesheets().add(fm.getFileFromResources("css/main.css").toURI().toString());
+		
+		stage.setScene(scene);
+		trayIconService.createFXTrayIcon(stage);
+		stage.show();
+	}
+
+	private void checkAlreadyRunning() throws ApplicationDejaEnCoursException {
+		try(var s = new ServerSocket(1044, 0, InetAddress.getByName("localhost"));) {
+			// teste juste la possibilité d'ouvrir un serveur à cette adresse
+		} catch (IOException e1) {
+			throw new ApplicationDejaEnCoursException();
+		}
+		var t = new Thread(() -> {
+			try(var server = new ServerSocket(1044, 0, InetAddress.getByName("localhost"));) {
+				server.accept();
+			} catch (IOException e) {
+				alertService.alert(e);
+			}
+		});
+		t.setDaemon(true);
+		t.start();
+	}
+}
