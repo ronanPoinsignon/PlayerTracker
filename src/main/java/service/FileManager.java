6f31bf9cd06507c2603f683dcdf2cbda76e3209b
Ronan <ronan.poinsignon@laposte.net>
2022-07-31 11:02:36 +0200

    refactor save/load service

--- src/main/java/service/FileManager.java
+++ src/main/java/service/FileManager.java
@@ -1,10 +1,20 @@
 package service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javafx.scene.image.Image;
@@ -65,6 +75,53 @@ public class FileManager implements IService {
 			throw new IllegalArgumentException(dictionnaire.getGetInputStreamFromResourceFileNotFound().getValue() + " : " + fileName);
 		} else {
 			return inputStream;
+		}
+	}
+
+	public void writeInto(final File file, final String content) throws FileNotFoundException, IOException {
+		if(!file.exists()) {
+			file.createNewFile();
+		}
+		Files.write(Paths.get(file.toURI()), content.getBytes(StandardCharsets.UTF_8));
+	}
+
+	public void writeInto(final File file, final Serializable object) throws FileNotFoundException, IOException {
+		try (var oos = new ObjectOutputStream(new FileOutputStream(file))) {
+			oos.writeObject(object);
+		}
+	}
+
+	public <T extends Serializable> void writeInto(final File file, final List<T> elements) throws FileNotFoundException, IOException {
+		try (var oos = new ObjectOutputStream(new FileOutputStream(file))) {
+			oos.writeInt(elements.size());
+
+			for(final T element : elements) {
+				oos.writeObject(element);
+			}
+		}
+	}
+
+	public String readFile(final File file) throws IOException {
+		return Files.readString(Paths.get(file.toURI()), StandardCharsets.UTF_8);
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends Serializable> T read(final File file) throws ClassNotFoundException, IOException {
+		try (var ois = new ObjectInputStream(new FileInputStream(file))) {
+			return (T) ois.readObject();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T extends Serializable> List<T> readList(final File file) throws ClassNotFoundException, IOException {
+		final List<T> elements = new ArrayList<>();
+		try (var ois = new ObjectInputStream(new FileInputStream(file))) {
+
+			for(var i = 1; i <= ois.readInt(); i++) {
+				elements.add((T) ois.readObject());
+			}
+
+			return elements;
 		}
 	}
 
