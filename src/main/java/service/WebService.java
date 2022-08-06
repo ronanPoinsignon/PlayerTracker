f18a1071663f21495622d549ba9374a048c10944
Ronan <ronan.poinsignon@laposte.net>
2022-08-07 00:43:25 +0200

    ajout des requête sur les nouvelles urls de l'api

--- src/main/java/service/WebService.java
+++ src/main/java/service/WebService.java
@@ -3,9 +3,17 @@ package service;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.google.gson.Gson;
 
 import modele.request.result.SummonerDataResult;
 import modele.request.result.SummonerInGameResult;
+import modele.request.result.SummonersDataResult;
+import modele.request.result.SummonersInGameResult;
 import modele.web.request.ClientWeb;
 
 public class WebService implements IService {
@@ -16,6 +24,10 @@ public class WebService implements IService {
 	private static final String GET_SUMMONER_BY_ID = "/summoner/getById/";
 	private static final String GET_SUMMONER_BY_NAME = "/summoner/getByName/";
 	private static final String GET_SUMMONER_GAME = "/summoner/getGame/";
+
+	private static final String GET_SUMMONER_BY_IDS = "/summoner/getByIds/";
+	private static final String GET_SUMMONER_BY_NAMES = "/summoner/getByNames/";
+	private static final String GET_SUMMONER_GAMES = "/summoner/getGames/";
 
 	private String baseUrl;
 
@@ -32,9 +44,35 @@ public class WebService implements IService {
 		return getValue(SummonerInGameResult.class, baseUrl + WebService.API + serverName + WebService.GET_SUMMONER_GAME + id);
 	}
 
+	public SummonersDataResult getSummonerByNames(final List<String> names, final String serverName) {
+		final List<String> newNames = names.stream().map(name -> URLEncoder.encode(name, StandardCharsets.UTF_8).replaceAll("\\+", "%20")).collect(Collectors.toList());
+		final var nameMap = new HashMap<>();
+		nameMap.put("names", new Gson().toJsonTree(newNames).getAsJsonArray().toString());
+		return postValue(SummonersDataResult.class, baseUrl + WebService.API + serverName + WebService.GET_SUMMONER_BY_NAMES, nameMap);
+	}
+
+	public SummonersDataResult getSummonerBySummonerIds(final List<String> ids, final String serverName) {
+		final var idMap = new HashMap<>();
+		idMap.put("ids", new Gson().toJsonTree(ids).getAsJsonArray().toString());
+		return postValue(SummonersDataResult.class, baseUrl + WebService.API + serverName + WebService.GET_SUMMONER_BY_IDS, idMap);
+	}
+
+	public SummonersInGameResult getSummonerGames(final List<String> ids, final String serverName) {
+		final var idMap = new HashMap<>();
+		idMap.put("ids", new Gson().toJsonTree(ids).getAsJsonArray().toString());
+		return postValue(SummonersInGameResult.class, baseUrl + WebService.API + serverName + WebService.GET_SUMMONER_GAMES, idMap);
+	}
+
 	private <T> T getValue(final Class<T> clazz, final String url) {
 		try {
 			return getClient(clazz).get(url);
+		} catch (IOException | InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	private <T> T postValue(final Class<T> clazz, final String url, final Map<Object, Object> params) {
+		try {
+			return getClient(clazz).post(url, params);
 		} catch (IOException | InterruptedException e) {
 			throw new RuntimeException(e);
 		}
