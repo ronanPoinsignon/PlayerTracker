807bb4a41fa9408bf7cc909edd01f4f8233a8ab4
Ronan <ronan.poinsignon@laposte.net>
2022-08-07 00:42:51 +0200

    ajout des classes de résultat pour les nouvelles url de l'api

---
+++ src/main/java/modele/request/result/SummonersDataResult.java
@@ -0,0 +1,9 @@
+package modele.request.result;
+
+import java.util.Map;
+
+import modele.request.data.SummonerData;
+
+public class SummonersDataResult extends RequestResult<Map<String, SummonerData>> {
+
+}
---
+++ src/main/java/modele/request/result/SummonersInGameResult.java
@@ -0,0 +1,9 @@
+package modele.request.result;
+
+import java.util.Map;
+
+import modele.request.data.SummonerInGame;
+
+public class SummonersInGameResult extends RequestResult<Map<String, SummonerInGame>>  {
+
+}
