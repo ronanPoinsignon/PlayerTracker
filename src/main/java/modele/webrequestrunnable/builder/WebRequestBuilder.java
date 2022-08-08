a99b987b0487283f3f40d288e0daec12d1ccfca9
Ronan <ronan.poinsignon@laposte.net>
2022-08-08 15:21:55 +0200

    suppression paramètre inutile

--- src/main/java/modele/webrequestrunnable/builder/WebRequestBuilder.java
+++ src/main/java/modele/webrequestrunnable/builder/WebRequestBuilder.java
@@ -1,6 +1,5 @@
 package modele.webrequestrunnable.builder;
 
-import modele.joueur.Joueur;
 import modele.webrequestrunnable.RequestPlayerData;
 
 public abstract class WebRequestBuilder<T extends RequestPlayerData> {
@@ -11,7 +10,7 @@ public abstract class WebRequestBuilder<T extends RequestPlayerData> {
 		return new WebRequestBuilderForPlayerList();
 	}
 
-	public static WebRequestBuilderForPlayer withPlayer(final Joueur joueur) {
+	public static WebRequestBuilderForPlayer withPlayer() {
 		return new WebRequestBuilderForPlayer();
 	}
 
