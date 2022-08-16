61de5ab69ce589e3d547ecfe320007b8d01e7237
Ronan <ronan.poinsignon@laposte.net>
2022-08-16 20:52:32 +0200

    update sort dans le mauvais ordre

--- src/main/java/modele/affichage/sortstrategy/ServerSort.java
+++ src/main/java/modele/affichage/sortstrategy/ServerSort.java
@@ -8,7 +8,7 @@ public class ServerSort<T extends Joueur> implements SortStrategy<T> {
 
 	@Override
 	public Comparator<T> getComparator() {
-		return Comparator.comparing(T::isInGame).thenComparing((j1, j2) -> {
+		return Comparator.comparing(T::isInGame).reversed().thenComparing((j1, j2) -> {
 			final var value1 = j1.getServer().getLabel().toLowerCase();
 			final var value2 = j2.getServer().getLabel().toLowerCase();
 
